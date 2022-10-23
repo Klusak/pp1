9f1230b8b45f456094cc33521e203a45de248730
Klusak <114917899+Klusak@users.noreply.github.com>
2022-10-23 16:02:43 +0200

    Update zadanie27.py

--- 03-ControlStructures/zadanie27.py
+++ 03-ControlStructures/zadanie27.py
@@ -1,4 +1,9 @@
-for i in range(6,-1,-3): 
-    for j in range(1,4): 
-        print(f" {i+j}",end="") 
+i=6
+j=1
+while i>=0:
+    while j<=3:
+        print(f"{i+j}",end=" ")
+        j+=1
+    i-=3
+    j=1
     print()
