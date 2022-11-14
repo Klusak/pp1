c49fcba6e22901d3d5ae36f918c8994c08030215
Klusak <225106@student.uek.krakow.pl>
2022-11-14 15:41:28 +0100

    zadania

---
+++ 06-Arrays/zadanie19.py
@@ -0,0 +1,5 @@
+def f(x,y,liczba):
+    if liczba>=x and liczba<=y:
+        return True
+    return False
+print(f(3,100,3))
---
+++ 06-Arrays/zadanie21.py
@@ -0,0 +1,5 @@
+def czywieksz(x,y):
+    if x<=y:
+        return False
+    return True
+print(czywieksz(3,1))
